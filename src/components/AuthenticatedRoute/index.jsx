d089a8d2248a9725f573fa47d7e3bbbaebc5b6e0
Aaron <aarondivi@gmail.com>
2023-05-08 13:19:56 +0200

    init

--- src/components/AuthenticatedRoute/index.jsx
+++ src/components/AuthenticatedRoute/index.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Auth from '../../contexts/Auth'
 import Website from '../../pages/Website'
-import NewPass from './pages/NewPass'
 
 const AuthenticatedRoute = ({ path, component }) => {
     
@@ -15,7 +14,6 @@ const AuthenticatedRoute = ({ path, component }) => {
     ) : (
         <Routes>
             <Route exact path='/' element={ <Website /> } />
-            <Route exact path='/updatePassword' element={ <NewPass /> } />
         </Routes>
     )
 }
